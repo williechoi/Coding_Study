cd31a39fa08316c20acd5024d0f990c7c05f95d4
Hyungsuk Choi <choihyungsuk@gmail.com>
2019-08-07 23:35:53 +0900

    another files added: introduction part

---
+++ Python/Introduction/Arithmetic_Operators.py
@@ -0,0 +1,16 @@
+# Hackerrank Language Proficiency Module: Python
+# Title: Arithmetic Operators
+# Directory: Python > Introduction > Arithmetic Operators
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
---
+++ Python/Introduction/Python_Division.py
@@ -0,0 +1,15 @@
+# Hackerrank Language Proficiency Module: Python
+# Title: Python: Division
+# Directory: Python > Introduction > Python: Division
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
