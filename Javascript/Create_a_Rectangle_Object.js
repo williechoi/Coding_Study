7de8464a704c2d8dc07c9caee69c792a4ed0bea1
Hyungsuk Choi <choihyungsuk@gmail.com>
2019-09-18 23:06:26 +0900

    learned how to create an object in Javascript

---
+++ Javascript/Create_a_Rectangle_Object.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function Rectangle(length, width){
+    this.length = length;
+    this.width = width;
+    this.perimeter = 2*this.length + 2*this.width;
+    this.area = this.length * this.width;
+}
+
+
+function main() {
+    const a = +(readLine());
+    const b = +(readLine());
+    
+    const rec = new Rectangle(a, b);
+    
+    console.log(rec.length);
+    console.log(rec.width);
+    console.log(rec.perimeter);
+    console.log(rec.area);
+}
