5fe4a481acc2ad42f5246d067e72d99913f3e8b7
Hyungsuk Choi <choihyungsuk@gmail.com>
2019-09-18 23:07:16 +0900

    learned how to use map function and lambda function in python:

---
+++ Python/Python Functionals/Map_and_Lambda_Function.py
@@ -0,0 +1,14 @@
+cube = lambda x: x ** 3
+
+
+def fibonacci(n):
+    fibo_list = [0, 1]
+    for i in range(2, n):
+        fibo_list.append(fibo_list[i-1] + fibo_list[i-2])
+
+    return fibo_list[0:n]
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
