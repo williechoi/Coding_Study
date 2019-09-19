bdf2dae9df0dbd91349e7dc41561454282c37829
Hyungsuk Choi <choihyungsuk@gmail.com>
2019-09-19 20:19:17 +0900

    files for how to make a loop of objects

---
+++ Javascript/Count_Objects.js
@@ -0,0 +1,57 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    var cnt = 0;
+    /*  
+     *  the variable objects is an array of objects, 
+     *  so we will use [] to access each element(object) of an array. 
+     */
+    for(var i = 0; i < objects.length; i++){
+        if(objects[i].x == objects[i].y){   //we count the number of objects in which two properties are the same 
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
